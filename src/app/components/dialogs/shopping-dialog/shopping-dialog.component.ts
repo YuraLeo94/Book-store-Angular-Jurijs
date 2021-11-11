af91c740c9a768c33de9f9445a05960a7844f66a
Leonovics <jurijs.leonovics@accenture.com>
2021-11-11 13:34:53 +0200

    Added return types and removed unused code

--- src/app/components/dialogs/shopping-dialog/shopping-dialog.component.ts
+++ src/app/components/dialogs/shopping-dialog/shopping-dialog.component.ts
@@ -1,31 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-shopping-dialog',
   templateUrl: './shopping-dialog.component.html',
   styleUrls: ['./shopping-dialog.component.less']
 })
-export class ShoppingDialogComponent implements OnInit {
-
-  faTimes = faTimes;
+export class ShoppingDialogComponent {
 
   constructor(public dialogRef: MatDialogRef<ShoppingDialogComponent>) { }
 
-  public ngOnInit(): void {
-  }
-
-  public onClose() {
+  public onClose():void {
     this.dialogRef.close();
   }
 
-  public onGoToCart() {
+  public onGoToCart():void {
     // TO DO https://trello.com/c/5GUVUqGA/18-implement-functionality-for-shopping-dialog
     console.log("onGoToCart");
   }
 
-  public onContinueShop() {
+  public onContinueShop():void {
     // TO DO https://trello.com/c/5GUVUqGA/18-implement-functionality-for-shopping-dialog
     console.log("onContinueShop");
   }
