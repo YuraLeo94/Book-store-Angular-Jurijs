84de581aa852ab690f0e97dcb2fd69ec92512c50
Leonovics <jurijs.leonovics@accenture.com>
2021-11-30 11:19:28 +0200

    Fixed double call of completed dialog  and moved the router to method

--- src/app/components/dialogs/completed-dialog/completed-dialog.component.ts
+++ src/app/components/dialogs/completed-dialog/completed-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-completed-dialog',
@@ -8,9 +9,10 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class CompletedDialogComponent {
 
-  constructor(public dialogRef: MatDialogRef<CompletedDialogComponent>) { }
+  constructor(public dialogRef: MatDialogRef<CompletedDialogComponent>, private router: Router) { }
 
   public onHomePage(): void {
+    this.router.navigate(['']);
     this.dialogRef.close();
   }
 
