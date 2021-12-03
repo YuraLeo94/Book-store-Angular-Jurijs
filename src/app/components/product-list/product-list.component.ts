7cd8e8f3baff86b940689b493ce2226f4f0b5e36
Leonovics <jurijs.leonovics@accenture.com>
2021-12-03 15:56:10 +0200

    Updated services with Behavior subject

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -14,7 +14,13 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService, private router: Router) { }
 
   public ngOnInit(): void {
-    this.productService.getProducts().subscribe(products => (this.products = products));
+    const productsData = this.productService.productsData;
+    if (productsData.length > 0) {
+      this.products = productsData;
+    } else {
+      this.productService.getProducts().subscribe(products => (this.products = products)).add(() => (this.productService.productsData = this.products));
+    }
+    // this.productService.getProducts().subscribe(products => (this.products = products));
   }
 
   public openProductDetail(product: Product): void {
